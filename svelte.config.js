3c3dbc13be23c9a81b571a0dc95b5e5b54f602dc
Sercan Sokmen <sercansokmen0@gmail.com>
2023-02-19 20:44:33 +0000

    mobile styling on works page

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
-
-// import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess  from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: preprocess ({}),
 
 	kit: {
